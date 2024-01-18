82f535a43af5ff2e65bb6e48936a1c397c0943ac
TevStark <tevkamau@gmail.com>
2024-01-19 01:17:08 +0300

    Complete Etch a Sketch Functionality

--- main.js
+++ main.js
@@ -4,24 +4,3 @@
 // gridSquare.classList.add('grid-squares')
 // container.appendChild(gridSquares);
 
-let container = document.getElementById('container');
-
-for(let i = 0; i < 256; i++){
-    let gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-squares');
-    gridSquare.setAttribute('id', 'grid-squares');
-    container.appendChild(gridSquare);
-    
-};
-
-let hoverEffect = document.querySelectorAll('#grid-squares');
-
-hoverEffect.forEach(function(hoverEffect){
-    hoverEffect.addEventListener('mouseover', function(){
-        this.style.backgroundColor = 'purple';
-    });
-    
-    hoverEffect.addEventListener('mouseout', function(){
-        this.style.backgroundColor = 'blueviolet';
-    });
-})
